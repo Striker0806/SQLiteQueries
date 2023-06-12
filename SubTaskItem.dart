7e34140f1161e161b127dfb6fdefcdd237a476f0
Rohith Kumar <130415522+Striker0806@users.noreply.github.com>
2023-06-12 21:19:52 +0530

    Update SubTaskItem.dart

--- SubTaskItem.dart
+++ SubTaskItem.dart
@@ -1,1 +1,20 @@
 //
+class SubtaskItem {
+  int? subtaskID;
+  String itemName;
+  int itemStatus;
+
+  SubtaskItem({
+    this.subtaskID,
+    required this.itemName,
+    this.itemStatus = 0,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'SubtaskID': subtaskID,
+      'ItemName': itemName,
+      'ItemStatus': itemStatus,
+    };
+  }
+}
